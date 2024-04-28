a6aed65f6ac6ba10f69ea7c0afd7562ea1f5d508
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2024-04-28 12:07:06 +0200

    keep only tests debug improvement and README new section from #388

--- entrypoint.ps1
+++
@@ -1,8 +0,0 @@
-[CmdletBinding()]
-Param(
-    [Parameter(Position = 0, ValueFromRemainingArguments = $true)]
-    [string] $Cmd
-)
-
-& 'C:/Program Files/OpenSSH-Win64/install-sshd.ps1'
-& C:/ProgramData/Jenkins/setup-sshd.ps1 $Cmd
