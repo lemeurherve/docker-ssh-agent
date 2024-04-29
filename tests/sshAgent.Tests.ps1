7d970f2d79e39a15944ab9452c680de37b097008
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2024-04-29 17:08:19 +0200

    fixup

--- tests/sshAgent.Tests.ps1
+++ tests/sshAgent.Tests.ps1
@@ -134,7 +134,7 @@ Describe "[$global:IMAGE_NAME] image has correct version of tools installed and 
 
         $exitCode, $stdout, $stderr = Run-Program 'docker' "exec $global:CONTAINERNAME $global:CONTAINERSHELL -C `"`$version = ssh -V 2>&1 ; Write-Host `$version`""
         $exitCode | Should -Be 0
-        $shortOpenSSHVersion = $global:OPENSSHVERSION.ToLower().Replace('v', '').Replace('.0', '').Replace('p-beta1', 'p1')  # Ex: V8.6.0.0p-Beta1 => 8.6p1
+        $shortOpenSSHVersion = $global:OPENSSHVERSION.ToLower().Replace('v', '').Replace('.0', '').Replace('p1-beta1', 'p1')  # Ex: V8.6.0.0p-Beta1 => 8.6p1
         Write-Host "shortOpenSSHVersion: $shortOpenSSHVersion"
         $stdout.Trim() | Should -Match "_$shortOpenSSHVersion"
     }
