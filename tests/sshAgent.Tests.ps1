6ce04574307ae3ef1609fc4c00ba2af3466f8c68
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2024-04-29 15:59:45 +0200

    proper nanoserver PATH

--- tests/sshAgent.Tests.ps1
+++ tests/sshAgent.Tests.ps1
@@ -129,14 +129,14 @@ Describe "[$global:IMAGE_NAME] image has correct version of tools installed and 
         $exitCode, $stdout, $stderr = Run-Program 'docker' "exec $global:CONTAINERNAME $global:CONTAINERSHELL -C `"if(`$null -eq (Get-Command ssh.exe -ErrorAction SilentlyContinue)) { exit -1 } else { exit 0 }`""
         $exitCode | Should -Be 0
 
-        $exitCode, $stdout, $stderr = Run-Program 'docker' "exec $global:CONTAINERNAME $global:CONTAINERSHELL -C `"`& ssh -V`""
+        $exitCode, $stdout, $stderr = Run-Program 'docker' "exec $global:CONTAINERNAME $global:CONTAINERSHELL -C `"`& ssh.exe -V`""
         $exitCode | Should -Be 0
         $stdout.Trim() | Should -Match "OpenSSH_${global:OPENSSHVERSION}"
     }
 
     It 'can connect via SSH to localhost' {
         $SSH_PORT=Get-Port $global:CONTAINERNAME 22
-        $exitCode, $stdout, $stderr = Run-Program 'docker' "exec $global:CONTAINERNAME $global:CONTAINERSHELL -C `"`& ssh -v jenkins@127.0.0.1 -p $SSH_PORT`""
+        $exitCode, $stdout, $stderr = Run-Program 'docker' "exec $global:CONTAINERNAME $global:CONTAINERSHELL -C `"`& ssh.exe -v jenkins@127.0.0.1 -p $SSH_PORT`""
         $exitCode | Should -Be 0
         $stdout.Trim() | Should -Match 'OpenSSH'
     }
